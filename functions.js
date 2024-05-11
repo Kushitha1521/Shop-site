cb0233e7c1061c6d91f26994aa98d685edba5960
Kushitha Lakshitha <168770766+Kushitha1521@users.noreply.github.com>
2024-05-11 12:31:38 +0530

    initial commit

---
+++ functions.js
@@ -0,0 +1,47 @@
+/*breakfast */
+function openNav1() {
+  document.getElementById("menu1").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "100%";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav1() {
+    document.getElementById("menu1").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+/*lunch */
+function openNav2() {
+    document.getElementById("menu2").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav2() {
+    document.getElementById("menu2").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+/*dinner */
+function openNav3() {
+    document.getElementById("menu3").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav3() {
+    document.getElementById("menu3").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+     }
+/*beverages*/
+
+function openNav4() {
+    document.getElementById("menu4").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "100%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav4() {
+    document.getElementById("menu4").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+
+            
